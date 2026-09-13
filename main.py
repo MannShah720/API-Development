8cf1cc080f6712607fd3e25698890fc737d8494f
Mann Shah <shahmann0108@gmail.com>
2026-09-13 22:57:43 +0100

    Create & Get posts

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+import random
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "post 1", "content": "content of post 1", "id": 1},
+    {"title": "post 2", "content": "content of post 2", "id": 2}
+]
 
 @app.get("/")
 def root():
@@ -10,9 +24,18 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "These are the posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = random.randint(1, 1000000)
+    my_posts.append(post_dict)
+    return {"new_post": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = next((post for post in my_posts if post['id'] == id), None)
+    if not post:
+        return {"error": "Post not found"}
+    return {"post_detail": post}
